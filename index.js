a627cb1954f0cb4081586556c523a1c0368413e6
Pau <frikeldon@gmail.com>
2020-12-09 17:43:44 +0100

    Correccion de nombre

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ export default {
   install (app) {
     for (const name in components) {
       const kebabName = name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
-      const componentName = 'standard-' + kebabName
+      const componentName = 'fluentui-' + kebabName
       app.component(componentName, components[name])
     }
   }
